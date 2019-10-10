96c288e8fb93b00bd3562cca55318c3358b46538
niehongyong <1902806374@qq.com>
2019-10-10 18:48:06 +0800

    修改图书信息后续增加内容

--- BMS-server/src/main/java/com/book/web/BookAddCommand.java
+++ BMS-server/src/main/java/com/book/web/BookAddCommand.java
@@ -7,118 +7,112 @@ import java.util.Date;
 
 public class BookAddCommand {
 
-
+    private int id;
     private String name;
+    private String cover;
     private String author;
-    private String publish;
-    private String isbn;
-    private String introduction;
-    private String language;
-    private BigDecimal price;
-    private Date pubdate;
-    private int classId;
-    private int pressmark;
-    private int state;
+    private String clazz;
+    private boolean isRent;
+    private String publisher;
+    private String publishDate;
+    private String price;
+    private float rate;
+    private String ISBN;
+    private String description;
 
-    public void setName(String name) {
-        this.name = name;
+    public int getId() {
+        return id;
     }
 
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public void setClassId(int classId) {
-        this.classId = classId;
-    }
-
-    public void setLanguage(String language) {
-        this.language = language;
-    }
-
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }
-
-
-
-    public void setPubdate(String pubdate) {
-        SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
-
-        try{
-            java.util.Date date=sdf.parse(pubdate);
-            this.pubdate=date;
-        }catch (ParseException e){
-            e.printStackTrace();
-        }
-
-    }
-
-    public void setPublish(String publish) {
-        this.publish = publish;
-    }
-
-    public void setIntroduction(String introduction) {
-        this.introduction = introduction;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-
-    public void setPressmark(int pressmark) {
-        this.pressmark = pressmark;
-    }
-
-    public void setState(int state) {
-        this.state = state;
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getName() {
         return name;
     }
 
-    public BigDecimal getPrice() {
-        return price;
+    public void setName(String name) {
+        this.name = name;
     }
 
-
-    public int getClassId() {
-        return classId;
+    public String getCover() {
+        return cover;
     }
 
-    public Date getPubdate() {
-        return pubdate;
+    public void setCover(String cover) {
+        this.cover = cover;
     }
 
     public String getAuthor() {
         return author;
     }
 
-    public String getIntroduction() {
-        return introduction;
+    public void setAuthor(String author) {
+        this.author = author;
     }
 
-    public int getPressmark() {
-        return pressmark;
+    public String getClazz() {
+        return clazz;
     }
 
-    public String getIsbn() {
-        return isbn;
+    public void setClazz(String clazz) {
+        this.clazz = clazz;
     }
 
-    public String getLanguage() {
-        return language;
+    public boolean isRent() {
+        return isRent;
     }
 
-    public int getState() {
-        return state;
+    public void setRent(boolean rent) {
+        isRent = rent;
     }
 
-    public String getPublish() {
-        return publish;
+    public String getPublisher() {
+        return publisher;
     }
 
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
 
+    public String getPublishDate() {
+        return publishDate;
+    }
 
+    public void setPublishDate(String publishDate) {
+        this.publishDate = publishDate;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public float getRate() {
+        return rate;
+    }
+
+    public void setRate(float rate) {
+        this.rate = rate;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
--- BMS-server/src/main/java/com/book/web/BookController.java
+++ BMS-server/src/main/java/com/book/web/BookController.java
@@ -22,32 +22,32 @@ public class BookController {
     }
 
     @RequestMapping("/querybook.html")
-    public ModelAndView queryBookDo(HttpServletRequest request,String searchWord){
-        boolean exist=bookService.matchBook(searchWord);
-        if (exist){
+    public ModelAndView queryBookDo(HttpServletRequest request, String searchWord) {
+        boolean exist = bookService.matchBook(searchWord);
+        if (exist) {
             ArrayList<Book> books = bookService.queryBook(searchWord);
             ModelAndView modelAndView = new ModelAndView("admin_books");
-            modelAndView.addObject("books",books);
+            modelAndView.addObject("books", books);
             return modelAndView;
-        }
-        else{
-            return new ModelAndView("admin_books","error","没有匹配的图书");
+        } else {
+            return new ModelAndView("admin_books", "error", "没有匹配的图书");
         }
     }
+
     @RequestMapping("/reader_querybook.html")
-    public ModelAndView readerQueryBook(){
-       return new ModelAndView("reader_book_query");
+    public ModelAndView readerQueryBook() {
+        return new ModelAndView("reader_book_query");
 
     }
+
     @RequestMapping("/reader_querybook_do.html")
-    public String readerQueryBookDo(HttpServletRequest request,String searchWord,RedirectAttributes redirectAttributes){
-        boolean exist=bookService.matchBook(searchWord);
-        if (exist){
+    public String readerQueryBookDo(HttpServletRequest request, String searchWord, RedirectAttributes redirectAttributes) {
+        boolean exist = bookService.matchBook(searchWord);
+        if (exist) {
             ArrayList<Book> books = bookService.queryBook(searchWord);
             redirectAttributes.addFlashAttribute("books", books);
             return "redirect:/reader_querybook.html";
-        }
-        else{
+        } else {
             redirectAttributes.addFlashAttribute("error", "没有匹配的图书！");
             return "redirect:/reader_querybook.html";
         }
@@ -55,95 +55,94 @@ public class BookController {
     }
 
     @RequestMapping("/allbooks.html")
-    public ModelAndView allBook(){
-        ArrayList<Book> books=bookService.getAllBooks();
-        ModelAndView modelAndView=new ModelAndView("admin_books");
-        modelAndView.addObject("books",books);
+    public ModelAndView allBook() {
+        ArrayList<Book> books = bookService.getAllBooks();
+        ModelAndView modelAndView = new ModelAndView("admin_books");
+        modelAndView.addObject("books", books);
         return modelAndView;
     }
-    @RequestMapping("/deletebook.html")
-    public String deleteBook(HttpServletRequest request,RedirectAttributes redirectAttributes){
-        long bookId=Integer.parseInt(request.getParameter("bookId"));
-        int res=bookService.deleteBook(bookId);
 
-        if (res==1){
+    @RequestMapping("/deletebook.html")
+    public String deleteBook(HttpServletRequest request, RedirectAttributes redirectAttributes) {
+        long bookId = Integer.parseInt(request.getParameter("bookId"));
+        int res = bookService.deleteBook(bookId);
+
+        if (res == 1) {
             redirectAttributes.addFlashAttribute("succ", "图书删除成功！");
             return "redirect:/allbooks.html";
-        }else {
+        } else {
             redirectAttributes.addFlashAttribute("error", "图书删除失败！");
             return "redirect:/allbooks.html";
         }
     }
 
     @RequestMapping("/book_add.html")
-    public ModelAndView addBook(HttpServletRequest request){
+    public ModelAndView addBook(HttpServletRequest request) {
 
-            return new ModelAndView("admin_book_add");
+        return new ModelAndView("admin_book_add");
 
     }
 
     @RequestMapping("/book_add_do.html")
-    public String addBookDo(BookAddCommand bookAddCommand,RedirectAttributes redirectAttributes){
-        Book book=new Book();
-        book.setBookId(0);
-        book.setPrice(bookAddCommand.getPrice());
-        book.setState(bookAddCommand.getState());
-        book.setPublish(bookAddCommand.getPublish());
-        book.setPubdate(bookAddCommand.getPubdate());
+    public String addBookDo(BookAddCommand bookAddCommand, RedirectAttributes redirectAttributes) {
+        Book book = new Book();
+        book.setId(bookAddCommand.getId());
         book.setName(bookAddCommand.getName());
-        book.setIsbn(bookAddCommand.getIsbn());
-        book.setClassId(bookAddCommand.getClassId());
+        book.setCover(bookAddCommand.getCover());
+        book.setClazz(bookAddCommand.getClazz());
         book.setAuthor(bookAddCommand.getAuthor());
-        book.setIntroduction(bookAddCommand.getIntroduction());
-        book.setPressmark(bookAddCommand.getPressmark());
-        book.setLanguage(bookAddCommand.getLanguage());
+        book.setPublisher(bookAddCommand.getPublisher());
+        book.setPublishDate(bookAddCommand.getPublishDate());
+        book.setPrice(bookAddCommand.getPrice());
+        book.setRate(bookAddCommand.getRate());
+        book.setDescription(bookAddCommand.getDescription());
+        book.setISBN(bookAddCommand.getISBN());
+        book.setRent(bookAddCommand.isRent());
 
 
-        boolean succ=bookService.addBook(book);
-        ArrayList<Book> books=bookService.getAllBooks();
-        if (succ){
+        boolean succ = bookService.addBook(book);
+        ArrayList<Book> books = bookService.getAllBooks();
+        if (succ) {
             redirectAttributes.addFlashAttribute("succ", "图书添加成功！");
             return "redirect:/allbooks.html";
-        }
-        else {
+        } else {
             redirectAttributes.addFlashAttribute("succ", "图书添加失败！");
             return "redirect:/allbooks.html";
         }
     }
 
     @RequestMapping("/updatebook.html")
-    public ModelAndView bookEdit(HttpServletRequest request){
-        long bookId=Integer.parseInt(request.getParameter("bookId"));
-        Book book=bookService.getBook(bookId);
-        ModelAndView modelAndView=new ModelAndView("admin_book_edit");
-        modelAndView.addObject("detail",book);
+    public ModelAndView bookEdit(HttpServletRequest request) {
+        long bookId = Integer.parseInt(request.getParameter("bookId"));
+        Book book = bookService.getBook(bookId);
+        ModelAndView modelAndView = new ModelAndView("admin_book_edit");
+        modelAndView.addObject("detail", book);
         return modelAndView;
     }
 
     @RequestMapping("/book_edit_do.html")
-    public String bookEditDo(HttpServletRequest request,BookAddCommand bookAddCommand,RedirectAttributes redirectAttributes){
-        long bookId=Integer.parseInt( request.getParameter("id"));
-        Book book=new Book();
-        book.setBookId(bookId);
-        book.setPrice(bookAddCommand.getPrice());
-        book.setState(bookAddCommand.getState());
-        book.setPublish(bookAddCommand.getPublish());
-        book.setPubdate(bookAddCommand.getPubdate());
+    public String bookEditDo(HttpServletRequest request, BookAddCommand bookAddCommand, RedirectAttributes redirectAttributes) {
+        long bookId = Integer.parseInt(request.getParameter("id"));
+        Book book = new Book();
+        book.setId((int) bookId);
         book.setName(bookAddCommand.getName());
-        book.setIsbn(bookAddCommand.getIsbn());
-        book.setClassId(bookAddCommand.getClassId());
+        book.setCover(bookAddCommand.getCover());
+        book.setClazz(bookAddCommand.getClazz());
         book.setAuthor(bookAddCommand.getAuthor());
-        book.setIntroduction(bookAddCommand.getIntroduction());
-        book.setPressmark(bookAddCommand.getPressmark());
-        book.setLanguage(bookAddCommand.getLanguage());
+        book.setPublisher(bookAddCommand.getPublisher());
+        book.setPublishDate(bookAddCommand.getPublishDate());
+        book.setPrice(bookAddCommand.getPrice());
+        book.setRate(bookAddCommand.getRate());
+        book.setDescription(bookAddCommand.getDescription());
+        book.setISBN(bookAddCommand.getISBN());
+        book.setRent(bookAddCommand.isRent());
 
 
-        boolean succ=bookService.editBook(book);
-        if (succ){
+        boolean succ = bookService.editBook(book);
+        if (succ) {
             redirectAttributes.addFlashAttribute("succ", "图书修改成功！");
             return "redirect:/allbooks.html";
-        }
-        else {
+        } else {
             redirectAttributes.addFlashAttribute("error", "图书修改失败！");
             return "redirect:/allbooks.html";
         }
@@ -151,25 +150,23 @@ public class BookController {
 
 
     @RequestMapping("/bookdetail.html")
-    public ModelAndView bookDetail(HttpServletRequest request){
-        long bookId=Integer.parseInt(request.getParameter("bookId"));
-        Book book=bookService.getBook(bookId);
-        ModelAndView modelAndView=new ModelAndView("admin_book_detail");
-        modelAndView.addObject("detail",book);
+    public ModelAndView bookDetail(HttpServletRequest request) {
+        long bookId = Integer.parseInt(request.getParameter("bookId"));
+        Book book = bookService.getBook(bookId);
+        ModelAndView modelAndView = new ModelAndView("admin_book_detail");
+        modelAndView.addObject("detail", book);
         return modelAndView;
     }
-
 
 
     @RequestMapping("/readerbookdetail.html")
-    public ModelAndView readerBookDetail(HttpServletRequest request){
-        long bookId=Integer.parseInt(request.getParameter("bookId"));
-        Book book=bookService.getBook(bookId);
-        ModelAndView modelAndView=new ModelAndView("reader_book_detail");
-        modelAndView.addObject("detail",book);
+    public ModelAndView readerBookDetail(HttpServletRequest request) {
+        long bookId = Integer.parseInt(request.getParameter("bookId"));
+        Book book = bookService.getBook(bookId);
+        ModelAndView modelAndView = new ModelAndView("reader_book_detail");
+        modelAndView.addObject("detail", book);
         return modelAndView;
     }
-
 
 
 }
